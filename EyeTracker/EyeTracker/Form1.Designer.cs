455692c7d65b53a758e056b0a330f1ce5619b9c6
Skiper527 <32278726+Skiper527@users.noreply.github.com>
2018-10-20 12:07:13 +0300

    Release

--- EyeTracker/EyeTracker/Form1.Designer.cs
+++ EyeTracker/EyeTracker/Form1.Designer.cs
@@ -36,9 +36,21 @@
             this.radioButton_date_image = new System.Windows.Forms.RadioButton();
             this.radioButton_date_point = new System.Windows.Forms.RadioButton();
             this.button_connect_camera = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.lPort = new System.Windows.Forms.Label();
+            this.lIP = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.chClients = new System.Windows.Forms.CheckedListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_eye)).BeginInit();
             this.panel1.SuspendLayout();
+            this.groupBox_server.SuspendLayout();
             this.groupBox_camera.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox_eye
@@ -46,6 +58,7 @@
             this.pictureBox_eye.Location = new System.Drawing.Point(1, 1);
             this.pictureBox_eye.Name = "pictureBox_eye";
             this.pictureBox_eye.Size = new System.Drawing.Size(574, 351);
+            this.pictureBox_eye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox_eye.TabIndex = 0;
             this.pictureBox_eye.TabStop = false;
             // 
@@ -61,12 +74,21 @@
             // groupBox_server
             // 
             this.groupBox_server.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.groupBox_server.Location = new System.Drawing.Point(267, 3);
+            this.groupBox_server.Controls.Add(this.btnStart);
+            this.groupBox_server.Controls.Add(this.btnConnect);
+            this.groupBox_server.Controls.Add(this.groupBox6);
+            this.groupBox_server.Controls.Add(this.tbName);
+            this.groupBox_server.Controls.Add(this.lIP);
+            this.groupBox_server.Controls.Add(this.label3);
+            this.groupBox_server.Controls.Add(this.lPort);
+            this.groupBox_server.Controls.Add(this.tbIP);
+            this.groupBox_server.Controls.Add(this.tbPort);
+            this.groupBox_server.Location = new System.Drawing.Point(173, 3);
             this.groupBox_server.Name = "groupBox_server";
-            this.groupBox_server.Size = new System.Drawing.Size(304, 114);
+            this.groupBox_server.Size = new System.Drawing.Size(398, 114);
             this.groupBox_server.TabIndex = 1;
             this.groupBox_server.TabStop = false;
-            this.groupBox_server.Text = "Option connect";
+            this.groupBox_server.Text = "Option connect network";
             // 
             // groupBox_camera
             // 
@@ -77,7 +99,7 @@
             this.groupBox_camera.Controls.Add(this.button_connect_camera);
             this.groupBox_camera.Location = new System.Drawing.Point(3, 3);
             this.groupBox_camera.Name = "groupBox_camera";
-            this.groupBox_camera.Size = new System.Drawing.Size(258, 111);
+            this.groupBox_camera.Size = new System.Drawing.Size(164, 111);
             this.groupBox_camera.TabIndex = 0;
             this.groupBox_camera.TabStop = false;
             this.groupBox_camera.Text = "Option camera";
@@ -85,7 +107,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 65);
+            this.label1.Location = new System.Drawing.Point(8, 45);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(135, 13);
             this.label1.TabIndex = 3;
@@ -94,7 +116,7 @@
             // radioButton_date_image
             // 
             this.radioButton_date_image.AutoSize = true;
-            this.radioButton_date_image.Location = new System.Drawing.Point(149, 88);
+            this.radioButton_date_image.Location = new System.Drawing.Point(8, 88);
             this.radioButton_date_image.Name = "radioButton_date_image";
             this.radioButton_date_image.Size = new System.Drawing.Size(104, 17);
             this.radioButton_date_image.TabIndex = 2;
@@ -105,7 +127,7 @@
             // 
             this.radioButton_date_point.AutoSize = true;
             this.radioButton_date_point.Checked = true;
-            this.radioButton_date_point.Location = new System.Drawing.Point(149, 65);
+            this.radioButton_date_point.Location = new System.Drawing.Point(8, 65);
             this.radioButton_date_point.Name = "radioButton_date_point";
             this.radioButton_date_point.Size = new System.Drawing.Size(99, 17);
             this.radioButton_date_point.TabIndex = 1;
@@ -123,6 +145,99 @@
             this.button_connect_camera.UseVisualStyleBackColor = true;
             this.button_connect_camera.Click += new System.EventHandler(this.button_connect_camera_Click);
             // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(68, 42);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(158, 20);
+            this.tbName.TabIndex = 19;
+            this.tbName.Text = "Eye";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Name";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(41, 16);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(88, 20);
+            this.tbIP.TabIndex = 17;
+            this.tbIP.Text = "127.0.0.1";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(176, 16);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(50, 20);
+            this.tbPort.TabIndex = 16;
+            this.tbPort.Text = "9000";
+            // 
+            // lPort
+            // 
+            this.lPort.AutoSize = true;
+            this.lPort.Location = new System.Drawing.Point(135, 19);
+            this.lPort.Name = "lPort";
+            this.lPort.Size = new System.Drawing.Size(29, 13);
+            this.lPort.TabIndex = 15;
+            this.lPort.Text = "Port:";
+            // 
+            // lIP
+            // 
+            this.lIP.AutoSize = true;
+            this.lIP.Location = new System.Drawing.Point(6, 19);
+            this.lIP.Name = "lIP";
+            this.lIP.Size = new System.Drawing.Size(20, 13);
+            this.lIP.TabIndex = 14;
+            this.lIP.Text = "IP:";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.chClients);
+            this.groupBox6.Location = new System.Drawing.Point(232, 11);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(160, 97);
+            this.groupBox6.TabIndex = 30;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Client";
+            // 
+            // chClients
+            // 
+            this.chClients.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chClients.FormattingEnabled = true;
+            this.chClients.Location = new System.Drawing.Point(6, 25);
+            this.chClients.Name = "chClients";
+            this.chClients.Size = new System.Drawing.Size(144, 64);
+            this.chClients.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(9, 68);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 23);
+            this.btnConnect.TabIndex = 31;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(126, 68);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 23);
+            this.btnStart.TabIndex = 32;
+            this.btnStart.Text = "Start sending";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -135,8 +250,11 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_eye)).EndInit();
             this.panel1.ResumeLayout(false);
+            this.groupBox_server.ResumeLayout(false);
+            this.groupBox_server.PerformLayout();
             this.groupBox_camera.ResumeLayout(false);
             this.groupBox_camera.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -151,6 +269,16 @@
         private System.Windows.Forms.RadioButton radioButton_date_image;
         private System.Windows.Forms.RadioButton radioButton_date_point;
         private System.Windows.Forms.Button button_connect_camera;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.CheckedListBox chClients;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lPort;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnConnect;
     }
 }
 
--- EyeTracker/EyeTracker/Form1.cs
+++ EyeTracker/EyeTracker/Form1.cs
@@ -11,6 +11,12 @@ using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
 
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Net;
+
+using NetManager;
+
 namespace EyeTracker
 {
     public partial class Form1 : Form
@@ -18,13 +24,25 @@ namespace EyeTracker
         public Form1()
         {
             InitializeComponent();
+
+            #region Client event
+            Client = new NMClient(this);
+            Client.OnError += new EventHandler<EventMsgArgs>(Client_OnError);
+            Client.OnStop += new EventHandler(Client_OnStop);
+            Client.OnNewClient += new EventHandler<EventClientArgs>(Client_OnNewClient);
+            Client.OnDeleteClient += new EventHandler<EventClientArgs>(Client_OnDeleteClient);
+            #endregion
         }
 
 
         #region variables
         bool flag_connect_camera = false;
+        bool flag_sending_message = false;
         VideoCaptureDevice videoSourse = null; //var choose device
         object object_lock = new object();
+        byte value_grad = 100;
+        NMClient Client;
+        byte[] image_array; //array is sending in network
         #endregion
 
 
@@ -37,7 +55,8 @@ namespace EyeTracker
         {
             if (flag_connect_camera)
             {
-                if (!(videoSourse == null) & (videoSourse.IsRunning))
+                if (!(videoSourse == null))
+                    if(videoSourse.IsRunning)
                 {
                     videoSourse.SignalToStop();
                     videoSourse = null;
@@ -74,7 +93,21 @@ namespace EyeTracker
         private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             lock (object_lock)
-                pictureBox_eye.Image = (Bitmap)eventArgs.Frame.Clone();
+            {
+                Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+                Point center_point = Detect_eye(bmp, value_grad);
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawLine(Pens.OrangeRed, new Point(center_point.X, center_point.Y - 20), new Point(center_point.X, center_point.Y + 20));
+                g.DrawLine(Pens.OrangeRed, new Point(center_point.X - 20, center_point.Y), new Point(center_point.X + 20, center_point.Y));
+                pictureBox_eye.Image = bmp;
+                g.Dispose();
+
+                if (radioButton_date_point.Checked)
+                    Send(center_point);
+                
+                if(radioButton_date_image.Checked)
+                    Send(image_array);
+            }
         }
 
 
@@ -85,11 +118,199 @@ namespace EyeTracker
         /// <param name="e"></param>
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            if (!(videoSourse == null) & (videoSourse.IsRunning))
+            if (!(videoSourse == null))
+                if (videoSourse.IsRunning)
             {
                 videoSourse.SignalToStop();
                 videoSourse = null;
             }
         }
+
+
+        /// <summary>
+        /// Search center eye
+        /// </summary>
+        /// <param name="bmp">Image eye</param>
+        /// <param name="value">Border black/white pixel </param>
+        /// <returns></returns>
+        private Point Detect_eye(Bitmap bmp, byte value)
+        {
+            System.Drawing.Point res_point = new System.Drawing.Point(1, 1);
+            int k_numeric = 0;
+
+            PixelFormat pxf = PixelFormat.Format24bppRgb;
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, pxf);
+            IntPtr ptr = bmpData.Scan0;
+            int numBytes = bmpData.Stride * bmp.Height;
+            int widthBytes = bmpData.Stride;
+            byte[] rgbValues = new byte[numBytes];
+            Marshal.Copy(ptr, rgbValues, 0, numBytes);
+
+
+            int x_sr = 0;
+            int y_sr = 0;
+            for (int counter = 0; counter < rgbValues.Length - 6; counter += 3)
+            {
+                if ((rgbValues[counter] < value) && (rgbValues[counter + 1] < value) && (rgbValues[counter + 2] < value))
+                {
+                    x_sr += (counter / 3) % bmp.Width;
+                    y_sr += (counter / 3) / bmp.Width;
+                    k_numeric++;
+                }
+            }
+
+            if (radioButton_date_image.Checked)
+                image_array = rgbValues;
+
+            Marshal.Copy(rgbValues, 0, ptr, numBytes);
+            bmp.UnlockBits(bmpData);
+
+            res_point.X = x_sr / k_numeric;
+            res_point.Y = y_sr / k_numeric;
+            return res_point;
+        }
+
+        #region Network function
+
+        /// <summary>
+        /// Send result point
+        /// </summary>
+        /// <param name="_point">result_point</param>
+        private void Send(Point _point)
+        {
+            int[] Addresses = new int[chClients.CheckedItems.Count];
+            for (int I = 0; I < chClients.CheckedItems.Count; I++)
+                Addresses[I] = (chClients.CheckedItems[I] as ClientAddress).Id;
+
+            byte[] Data = new byte[2 * sizeof(int)];
+
+            Array.Copy(BitConverter.GetBytes((int)18), Data, sizeof(int));
+            Array.Copy(BitConverter.GetBytes(_point.X), 0, Data, sizeof(int), sizeof(int));
+            Array.Copy(BitConverter.GetBytes(_point.Y), 0, Data, sizeof(int), sizeof(int));
+            Client.SendData(Addresses, Data);
+            DateTime dt = DateTime.Now;
+        }
+
+
+        /// <summary>
+        /// Send result image
+        /// </summary>
+        /// <param name="bmp_array">result_image</param>
+        private void Send(byte[] bmp_array)
+        {
+            int[] Addresses = new int[chClients.CheckedItems.Count];
+            for (int I = 0; I < chClients.CheckedItems.Count; I++)
+                Addresses[I] = (chClients.CheckedItems[I] as ClientAddress).Id;
+
+            byte[] Data = new byte[sizeof(int) + sizeof(int)*bmp_array.Length];
+
+            Array.Copy(BitConverter.GetBytes((int)18), Data, sizeof(int));
+            Array.Copy(bmp_array, 0, Data, sizeof(int), sizeof(int));
+            Client.SendData(Addresses, Data);
+            DateTime dt = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Error network
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Client_OnError(object sender, EventMsgArgs e)
+        {
+            MessageBox.Show(e.Msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Stop network
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Client_OnStop(object sender, EventArgs e)
+        {
+            tbIP.Enabled = true;
+            tbName.Enabled = true;
+            tbPort.Enabled = true;
+            btnConnect.Enabled = true;
+            btnConnect.Text = "Connect";
+
+            chClients.Items.Clear();
+        }
+
+        /// <summary>
+        /// Delete client network
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Client_OnDeleteClient(object sender, EventClientArgs e)
+        {
+            ClientAddress Cl = new ClientAddress(e.ClientId, e.Name);
+            int I = chClients.Items.Count - 1;
+            while ((I >= 0) && (Cl.ToString() != chClients.Items[I].ToString()))
+                I--;
+            if (I >= 0)
+                chClients.Items.RemoveAt(I);
+        }
+
+        /// <summary>
+        /// Add new client
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Client_OnNewClient(object sender, EventClientArgs e)
+        {
+            ClientAddress Cl = new ClientAddress(e.ClientId, e.Name);
+            chClients.Items.Add(Cl);
+        }
+
+        /// <summary>
+        /// Button start connect with network
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!Client.Running)
+            {
+                tbIP.Enabled = false;
+                tbName.Enabled = false;
+                tbPort.Enabled = false;
+                btnConnect.Text = "Disconnect";
+                btnStart.Enabled = true;
+
+                Client.Port = int.Parse(tbPort.Text);
+                Client.IPServer = IPAddress.Parse(tbIP.Text);
+                Client.Name = tbName.Text;
+                Client.RunClient();
+            }
+            else
+            {
+                btnConnect.Enabled = false;
+                btnStart.Enabled = false;
+                Client.StopClient();
+            }
+        }
+
+
+        /// <summary>
+        /// Button start sending message
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text == "Start sending")
+            {
+                flag_sending_message = true;
+                btnStart.Text = "Stop sending";
+            }
+            else
+            {
+                flag_sending_message = false;
+                btnStart.Text = "Start sending";
+            }
+        }
+
+        #endregion
     }
 }
